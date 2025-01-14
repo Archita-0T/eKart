ddad2614d0cd3ba0408711881eafd0d6983299b9
unknown <ATiwari@quark.com>
2025-01-14 18:55:17 +0530

    enhanced search bar functionality

--- src/app/product-list/search/search.component.ts
+++ src/app/product-list/search/search.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  searchText:string="Mens wear";
+  updateSearchText(event:any){
+    this.searchText=event.target.value;
+
+  }
 
 }
